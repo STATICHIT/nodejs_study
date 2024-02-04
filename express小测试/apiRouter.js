c20c3b71d65cc7347e2e6748f3a8af29eef9d9ba
STATICHIT <2394412110@qq.com>
2024-02-04 17:31:11 +0800

    2.4学习进度上传

--- express小测试/apiRouter.js
+++ express小测试/apiRouter.js
@@ -2,6 +2,12 @@
 const express = require('express')
 const apiRouter = express.Router()
 
+const jwt = require('jsonwebtoken')//导入用于生成JWT字符串的包
+const secretKey = 'STATICHIT 666 ovo'//定义secert密钥
+
+// expressJWT.expressjwt({ secret: secretKey, algorithms: ['HS256'] })
+// expressJWT.expressjwt({ secret: secretKey, algorithms: ['HS256'] }).unless({ path: '/^\/api\//' })
+
 //编写get接口
 apiRouter.get('/get', (req, res) => {
   //1.获取到客户端通过查询字符串，发送到服务器的数据
@@ -29,8 +35,56 @@ apiRouter.post('/post', (req, res) => {
 //编写delete接口
 apiRouter.delete('/delete', (req, res) => {
   res.send({
-    status: 0,          
-    msg: 'DELETE请求成功！', 
+    status: 0,
+    msg: 'DELETE请求成功！',
+  })
+})
+
+//登陆的API接口
+apiRouter.post('/login', (req, res) => {
+  //判断用户提交的登录信息是否正确
+  if (req.body.username != 'admin' || req.body.password != '000000') {
+    return res.send({ status: 1, msg: "登陆失败" })
+  }
+
+  // //将登陆成功后的用户信息，保存到Session中
+  // //注意：只有成功配置了express-session这个中间件之后，才能狗通过req点出来session这个属性
+  // req.session.user = req.body//用户信息
+  // req.session.isLogin = true//用户登陆状态
+  // console.log("session：", req.session)
+  // res.send({
+  //   status: 0,
+  //   msg: '登陆成功',
+  //   username: req.session.user.username,
+  // })
+
+  console.log("SS", req.body.username)
+  const tokenStr = jwt.sign({ user: req.body.username }, secretKey, { expiresIn: '3000s' })
+  res.send({
+    status: 0,
+    msg: '登陆成功',
+    token: tokenStr//要发送给客户端的token字符串
+  })
+})
+
+//退出登陆的API接口
+apiRouter.post('/logout', (req, res) => {
+  //清空Session信息
+  res.session.destroy()//调用req.session.destory()函数，即可清空服务器保存的session信息。
+  console.log("session：", req.session)
+  res.send({
+    status: 0,
+    msg: '退出登陆成功',
+  })
+})
+
+//权限接口
+apiRouter.post('/admin/jwt', (req, res) => {
+  const auth = req.auth
+  res.send({
+    status: 0,
+    msg: '权限接口测试成功',
+    data: auth
   })
 })
 module.exports = apiRouter
--- express小测试/app.js
+++ express小测试/app.js
@@ -1,22 +1,46 @@
-//导入express
-const express = require('express')
-
-//导入cors中间件
-const cors=require('cors')
-
-// 导入路由模块
-const apiRouter = require('./apiRouter.js')
-
+const express = require('express')//导入express
+const cors = require('cors')//导入cors中间件
+const session = require('express-session')//导入session中间件
+const apiRouter = require('./apiRouter.js')// 导入路由模块
+const expressJWT = require('express-jwt')//导入用于将Token还原成JSON对象的包
+const secretKey = 'STATICHIT 666 ovo'//定义secert密钥
 //创建服务器实例
 const app = express()
+app.use(session({
+  secret: 'statichit',    //secret属性的值可以为任意字符串
+  resave: false,          //固定写法
+  saveUninitialized: true //固定写法
+}))
 
 //注册express内置中间件：urlencoded(注意中间件注册要放在路由前面)
 app.use(express.urlencoded({ extended: false }))
 //注册cors中间件
 app.use(cors())
+
+app.use(
+  expressJWT.expressjwt({
+    secret: secretKey,
+    algorithms: ['HS256']
+  })
+)//要写在路由模块前面！！！！！！！！！
+
 //注册路由模块
 app.use('/api', apiRouter)
 
+//使用全局错误处理中间件，捕获解析JWT失败后产生的错误
+app.use((err, req, res, next) => {
+  //这次错误是token解析失败导致的
+  if (err.name === 'UnauthorizedError') {
+    return res.send({
+      status: 401,
+      message: "无效的token"
+    })
+  }
+  res.send({
+    status: 500,
+    message: "未知错误"
+  })
+})
 //启动服务器
 app.listen(80, () => {
   console.log("express server running at http://127.0.0.1")
